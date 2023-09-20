2395dc84021d417e146fceb45d63d2c4c977f0d6
Your Name <you@example.com>
2023-09-20 14:50:07 +0100

    final deplyoment

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
     return (
         <div className="navbar">
-  <a href="http://localhost:5173/articles">Home</a>
+  <a href="https://reliable-conkies-efec20.netlify.app/articles">Home</a>
   {/* <a href="http://localhost:5173/articles/topics/news">News</a> */}
   <div className="dropdown">
     <button className="dropbtn">Topics
